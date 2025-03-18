e064a68aade3332185fb5bbcc5838c26fcd7ea09
daniel7fontes <daniel.md.fontes@gmail.com>
2025-03-18 18:16:19 -0300

    Criando o repositório

---
+++ DPD_utils/NN_tools.py
@@ -0,0 +1,196 @@
+# -*- coding: utf-8 -*-
+"""
+======================================================================
+Funções para aplicação, treinamento e verificação de desempenho da NN-DPD
+======================================================================
+"""
+
+import numpy as np
+import torch as th
+
+from tqdm.notebook  import tqdm
+from scipy.constants import pi
+from torch           import nn
+
+from torch.utils.data         import Dataset, DataLoader
+from optic_private.torchDSP   import pnorm
+from optic_private.torchUtils import memoryLessDataSet, MLP, train_model, test_model, slidingWindowDataSet, fitFilterNN
+
+
+def NN_training(sigIn, sigRef, param, RoFChannel_model = None):
+    
+    num_feat = param.num_input
+    
+    N1 = param.N1
+    N2 = param.N2
+    
+    divByL        = param.divByL
+    trainTestFrac = param.trainTestFrac
+    batch_size    = param.batch_size
+    shuffle       = param.shuffle
+    includeMemory = param.includeMemory
+    Ntaps         = param.Ntaps
+    augment       = param.augment
+    
+    lr            = param.lr
+    epochs        = param.epochs
+    
+    pgrsBar       = param.pgrsBar
+    directLearn   = param.directLearn
+    device        = param.device
+
+    if directLearn:
+        train_dataloader, test_dataloader = createDatasets(sigRef, sigRef, divByL, trainTestFrac,\
+                                                           batch_size, includeMemory, Ntaps, augment=augment)
+
+        for p in RoFChannel_model.parameters():
+            p.requires_grad = False
+        
+        if includeMemory:
+            DPD_model = MLP([num_feat*Ntaps, N1, N2, 2], activation=nn.ReLU()).to(device)
+        else:
+            DPD_model = MLP([2, 32, 32, 2], activation=nn.ReLU()).to(device)
+        
+        
+        loss_fn = nn.MSELoss()
+        optimizer = th.optim.Adam(DPD_model.parameters(), lr = lr)
+        
+        trainLoss = np.zeros(epochs)
+        testLoss  = np.zeros(epochs)
+        
+        for t in tqdm(range(epochs), disable = not(pgrsBar)):
+            # Training
+            size = len(train_dataloader.dataset)
+            DPD_model.train()
+            
+            for batch, (X, y) in enumerate(train_dataloader):    
+                # Compute prediction error        
+                chInput = DPD_model(X)                
+                if includeMemory:
+                    chInput = th.view_as_complex(chInput)            
+                    chOutput = th.view_as_real(fitFilterNN(chInput, RoFChannel_model, Ntaps, 1, len(chInput), predict=False))
+                    loss = loss_fn(chOutput, y)
+                else:
+                    chOutput = RoFChannel_model(chInput)            
+                    loss = loss_fn(chOutput, X)
+        
+                # Backpropagation
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+        
+                if batch % 100 == 0:
+                    loss, current = loss.item(), (batch + 1) * len(X)
+                    #logg.info(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            
+            # Validation      
+            num_batches = len(test_dataloader)
+            DPD_model.eval()
+            test_loss = 0    
+            with th.no_grad():
+                for X, y in test_dataloader:           
+                    # Compute prediction error        
+                    chInput = DPD_model(X)           
+                    
+                    if includeMemory:
+                        chInput = th.view_as_complex(chInput)
+                        chOutput = th.view_as_real(fitFilterNN(chInput, RoFChannel_model, Ntaps, 1, len(chInput)))
+                        test_loss += loss_fn(chOutput, y).item()
+                    else:
+                        chOutput = RoFChannel_model(chInput)           
+                        test_loss += loss_fn(chOutput, X).item()          
+                                    
+            test_loss /= num_batches    
+            
+            trainLoss[t] = loss.item()
+            testLoss[t]  = test_loss
+            
+            if (t+1) % 100 == 0:
+                for g in optimizer.param_groups:
+                    g['lr'] = g['lr']/1.25
+
+    
+    else:
+        train_dataloader, test_dataloader = createDatasets(sigRef, sigIn, divByL, trainTestFrac,\
+                                                   batch_size, includeMemory, Ntaps, augment=augment)
+        
+        # Define neural network (multilayer perceptron - MLP) model
+        if includeMemory:
+            DPD_model = MLP([num_feat*Ntaps, N1, N2, 2], activation = nn.ReLU()).to(device)
+        else:
+            DPD_model = MLP([2, 32, 32, 2], activation = nn.ReLU()).to(device)
+        
+        loss_fn = nn.MSELoss()
+        optimizer = th.optim.Adam(DPD_model.parameters(), lr = lr)
+        
+        trainLoss = np.zeros(epochs)
+        testLoss  = np.zeros(epochs)
+        
+        for t in tqdm(range(epochs), disable = not(pgrsBar)): 
+            train_losses = train_model(train_dataloader, DPD_model, loss_fn, optimizer)
+            test_losses  = test_model(test_dataloader, DPD_model, loss_fn)    
+            
+            trainLoss[t] = np.mean(train_losses)
+            testLoss[t]  = np.mean(test_losses)
+            
+            if (t+1)%100 == 0:
+                for g in optimizer.param_groups:
+                    g['lr'] = g['lr']/2
+
+        
+    return DPD_model, trainLoss, testLoss
+
+
+
+def createDatasets(
+    sigIn,
+    sigRef,
+    divByL,
+    trainTestFrac,
+    batch_size,
+    includeMemory,
+    Ntaps,
+    shuffle=False,
+    augment=False
+):
+    sig_in  = pnorm(sigIn[0 : len(sigIn) // divByL])
+    sig_ref = pnorm(sigRef[0 : len(sigRef) // divByL])
+
+    # Create the dataset
+    indx_train = th.arange(0, int(trainTestFrac * len(sig_in)))
+    indx_test = th.arange(int(trainTestFrac * len(sig_in)), len(sig_in))
+
+    sig_train = sig_in[indx_train]  # get signal amplitude samples (L,)
+    sig_test = sig_in[indx_test]  # get signal amplitude samples (L,)
+
+    indy_train = th.arange(0, int(trainTestFrac * len(sig_ref)))
+    indy_test = th.arange(int(trainTestFrac * len(sig_ref)), len(sig_ref))
+       
+    if includeMemory:
+        train_dataset = slidingWindowDataSet(
+            sig_ref[indy_train], sig_train, Ntaps, augment=augment
+        )
+        test_dataset = slidingWindowDataSet(
+            sig_ref[indy_test], sig_test, Ntaps, augment=augment
+        )
+
+        # Create a data loader for batching and shuffling the data
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=shuffle
+        )
+        test_dataloader = DataLoader(
+            test_dataset, batch_size=batch_size, shuffle=shuffle
+        )
+    else:
+        train_dataset = memoryLessDataSet(sig_ref[indy_train], sig_train, augment=augment)
+        test_dataset = memoryLessDataSet(sig_ref[indy_test], sig_test, augment=augment)
+
+        # Create a data loader for batching and shuffling the data
+        train_dataloader = DataLoader(
+            train_dataset, batch_size=batch_size, shuffle=shuffle
+        )
+        test_dataloader = DataLoader(
+            test_dataset, batch_size=batch_size, shuffle=shuffle
+        )
+
+    return train_dataloader, test_dataloader
---
+++ DPD_utils/plots.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+"""
+======================================================================
+Funções para geração de figuras associadas à análise de resultados de DPD
+======================================================================
+"""
+
+import numpy as np
+import matplotlib.pytplot as plt
+
+
+def plotDPD_const(symbTx, symbRx, symbRx_DLA, symbRx_ILA, show = False, savefig = False, file_path = None):
+    
+    fig, axs = plt.subplots(1, 2, figsize = (16, 8))
+    
+    axs[0].plot(symbRx.real, symbRx.imag, "o", color = "r", ms = 2, label = "Sem DPD")
+    axs[0].plot(symbRx_DLA.real, symbRx_DLA.imag, "o", color = "b", ms = 2, label = "Com DPD")
+    axs[0].plot(symbTx.real, symbTx.imag, "o", color = "k",  ms = 3, label = "SymbTx")
+    axs[0].set_title("DPD - DLA", fontsize = 18)
+    axs[0].set_ylabel("Quadrature - Q")
+    axs[0].set_xlabel("In-Phase - I")
+    axs[0].set_xlim(-1.5, 1.5)
+    axs[0].set_ylim(-1.5, 1.5)
+    axs[0].grid()
+    
+    axs[1].plot(symbRx.real, symbRx.imag, "o", color = "r", ms = 2, label = "Sem DPD")
+    axs[1].plot(symbRx_ILA.real, symbRx_ILA.imag, "o", color = "b", ms = 2, label = "Com DPD")
+    axs[1].plot(symbTx.real, symbTx.imag, "o", color = "k",  ms = 3, label = "SymbTx")
+    axs[1].set_title("DPD - ILA", fontsize = 18)
+    axs[1].set_ylabel("Quadrature - Q")
+    axs[1].set_xlabel("In-Phase - I")
+    axs[1].set_xlim(-1.5, 1.5)
+    axs[1].set_ylim(-1.5, 1.5)
+    axs[1].grid()
+    
+    plt.tight_layout()
+    if savefig:
+        plt.savefig(file_path)
+    
+    if not(show):
+        plt.close()
+
+
+def plotDPD_spec(freq, P_sigTx, P_sigRx_PA, P_sigRx_PA_DLA, P_sigRx_PA_ILA, Rs, show = False, savefig = False, file_path = None):    
+    fig, axs = plt.subplots(1, 2, figsize = (16, 6))
+    
+    axs[0].plot(freq/1e9, 10*np.log10(P_sigTx), color = "k", ls = "-", lw = 2, label = "Tx")
+    axs[0].plot(freq/1e9, 10*np.log10(P_sigRx_PA), color = "r", ls = "--", lw = 2, label = "Sem DPD")
+    axs[0].plot(freq/1e9, 10*np.log10(P_sigRx_PA_DLA), color = "b", ls = "--", lw = 2, label = r"Com DPD-DLA")
+    axs[0].set_title("DPD - DLA", fontsize = 18)
+    axs[0].set_xlim(-1.5*Rs/1e9, 1.5*Rs/1e9)
+    axs[0].set_ylim(-125, -80)
+    axs[0].set_xlabel("f [GHz]")
+    axs[0].set_ylabel("Power Spectral Density [dB/Hz]")
+    axs[0].legend(fontsize = 14, framealpha = 1)
+    axs[0].grid()
+    
+    axs[1].plot(freq/1e9, 10*np.log10(P_sigTx), color = "k", ls = "-", lw = 2, label = "Tx")
+    axs[1].plot(freq/1e9, 10*np.log10(P_sigRx_PA), color = "r", ls = "--", lw = 2, label = "Sem DPD")
+    axs[1].plot(freq/1e9, 10*np.log10(P_sigRx_PA_ILA), color = "b", ls = "--", lw = 2, label = r"Com DPD-ILA")
+    axs[1].set_title("DPD - ILA", fontsize = 18)
+    axs[1].set_xlim(-1.5*Rs/1e9, 1.5*Rs/1e9)
+    axs[1].set_ylim(-125, -80)
+    axs[1].set_xlabel("f [GHz]")
+    axs[1].set_ylabel("Power Spectral Density [dB/Hz]")
+    axs[1].legend(fontsize = 14, framealpha = 1)
+    axs[1].grid()
+    
+    plt.tight_layout()
+    if savefig:
+        plt.savefig(file_path)
+    
+    if not(show):
+        plt.close()
+
+
+def plotDPD_SNR(SNR_per_carrier, SNR_per_carrier_DPD, Ns, DPD_type, show = False, savefig = False, file_path = None):    
+    fig, axs = plt.subplots(figsize = (10, 5))
+    
+    axs.plot(SNR_per_carrier, color = "r", label = "Sem DPD")
+    axs.plot(SNR_per_carrier_DPD, color = "b", label = f"Com DPD - {DPD_type}")
+    axs.set_xlim(0, Ns)
+    axs.set_ylabel("SNR [dB]")
+    axs.set_xlabel("Carrier")
+    axs.legend(framealpha = 1, fontsize = 14)
+    axs.grid()
+    plt.tight_layout()
+    
+    if savefig:
+        plt.savefig(file_path)
+    
+    if not(show):
+        plt.close()
